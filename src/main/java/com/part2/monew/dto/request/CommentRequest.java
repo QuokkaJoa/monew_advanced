f9618efaccbe03f2a9dc2575d40b1d7de2464342
쿼카좋아 <a939515@gmail.com>
2025-12-02 21:19:11 +0900

    refactor: CommentRequest를 record 클래스로 변경 후 Builder 추가

--- src/main/java/com/part2/monew/dto/request/CommentRequest.java
+++ src/main/java/com/part2/monew/dto/request/CommentRequest.java
@@ -1,39 +1,20 @@
 package com.part2.monew.dto.request;
 
 import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.sql.Timestamp;
 import java.util.UUID;
 
-@Getter
-@Setter
-@NoArgsConstructor
-public class CommentRequest {
-    private UUID articleId;
-
-    private String orderBy;
-
-    private String direction;
-
-    private String cursor;
-
-    private Timestamp after;
-
-    private Integer limit;
-
-    private UUID requestUserId;
-
+public record CommentRequest(
+    UUID articleId,
+    String orderBy,
+    String direction,
+    Integer limit,
+    String cursor,
+    Timestamp after,
+    UUID requestUserId
+) {
     @Builder
-    private CommentRequest(UUID articleId, String orderBy, String direction, String cursor, Timestamp after, Integer limit, UUID requestUserId) {
-        this.articleId = articleId;
-        this.orderBy = orderBy;
-        this.direction = direction;
-        this.cursor = cursor;
-        this.after = after;
-        this.limit = limit;
-        this.requestUserId = requestUserId;
+    public CommentRequest {
     }
 }
