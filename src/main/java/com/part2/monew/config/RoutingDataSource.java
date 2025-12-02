f348fd49429a5c614a60043b6a0c8cee19a60896
쿼카좋아 <a939515@gmail.com>
2025-12-02 21:23:20 +0900

    refactor: 레플리케이션 동작 어노테이션을 Master, ReadOnly -> 기존 Spring Transaction을 따르게 변경

--- src/main/java/com/part2/monew/config/RoutingDataSource.java
+++ src/main/java/com/part2/monew/config/RoutingDataSource.java
@@ -3,13 +3,16 @@ package com.part2.monew.config;
 import com.part2.monew.entity.DataSourceType;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
 
 @Slf4j
 public class RoutingDataSource extends AbstractRoutingDataSource {
 
     @Override
     protected Object determineCurrentLookupKey() {
-        DataSourceType type = DataSourceContextHolder.get();
-        return type == null ? DataSourceType.MAIN : type;
+        boolean isReadOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+
+        return isReadOnly ? DataSourceType.STANDBY : DataSourceType.MAIN;
     }
 }
